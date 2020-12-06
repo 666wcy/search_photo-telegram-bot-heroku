8e80374bc07652f0b53a5fffd5bd5e843097c13a
666wcy <769020367@qq.com>
2020-12-06 21:03:13 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -P https://github.com/666wcy/seach_photo-telegram-bot-heroku/raw/master/bot.py
+wget -P / https://github.com/666wcy/seach_photo-telegram-bot-heroku/raw/master/bot.py
 python bot.py
 tail -f /dev/null	
