10e201b24418dd6bbf05a3dbdd2a7e6d3313ebf3
666wcy <769020367@qq.com>
2020-12-06 21:35:27 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ import os
 
 # Example of your code beginning
 #           Config vars
-token = os.environ['TOKEN']
+token = os.environ['TELEGRAM_TOKEN']
 
 session = requests.session()
 bot = telebot.TeleBot(token)
