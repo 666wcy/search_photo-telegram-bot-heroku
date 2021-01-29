de265ee994bc24e3d695da9da3e58fbd68e01fdf
666wcy <769020367@qq.com>
2021-01-29 11:18:46 +0800

    新增唤醒

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask
-
+from bot import *
 
 app = Flask(__name__)
 
@@ -12,12 +12,18 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return 'Hello World!'
+    while True:
+        try:
+            bot.polling(none_stop=True)
+        except Exception as e:
+            time.sleep(2)
+
+    
 
 
-
+#worker: python3 bot.py
 
 if __name__ == '__main__':
-    
+
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
