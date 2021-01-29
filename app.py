ba00953cca86f83fcfdf58885451369d4f95cf89
666wcy <769020367@qq.com>
2021-01-29 16:46:53 +0800

    新增检查状态

--- app.py
+++ app.py
@@ -1,25 +1,57 @@
-import os
 
-from flask import Flask
+
+
 from bot import *
 
+import threading
+from flask import Flask
+status =""
 app = Flask(__name__)
 
+@app.route('/bot', methods=['GET'])
+def bot_info():
+    try:
+        print(bot.get_me())
+        return str(bot.get_me())
+    except:
+        return "无法获取bot信息，请检查api token"
 
+def run_bot():
+
+    while True:
+        try:
+            bot.polling(none_stop=True)
+        except Exception as e:
+            time.sleep(10)
 
 
 
 
 @app.route('/', methods=['GET'])
 def index():
-    while True:
-        try:
-            bot.polling(none_stop=True)
-        except Exception as e:
-            time.sleep(2)
+    global status
+    if status=="":
+        t1 = threading.Thread(target=run_bot)  # 通过target指定子线程要执行的任务。可以通过args=元组 来指定test1的参数。
 
-    
+        t1.start()  # 只有在调用start方法后才会创建子线程并执行
 
+        print(t1.is_alive())
+        status=t1
+        # threading.enumerate()  打印正在执行的线程,包括主线程和子线程
+        #print(threading.enumerate())
+        return "正在唤醒Bot", 200
+    else:
+        print(status.is_alive())
+        if status.is_alive()==True:
+            return "Bot 已经在运行", 200
+        elif status.is_alive()==False:
+            t1 = threading.Thread(target=run_bot)  # 通过target指定子线程要执行的任务。可以通过args=元组 来指定test1的参数。
+
+            t1.start()  # 只有在调用start方法后才会创建子线程并执行
+
+            print(t1.is_alive())
+            status=t1
+            return "重新唤醒Bot", 200
 
 #worker: python3 bot.py
 
